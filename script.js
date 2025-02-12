1dbaa45af2bfb6f0727ab6b429485fa54b4468ce
YepaProg <papaulygamer@gmail.com>
2025-02-12 11:49:58 +0100

    Optimized js

--- script.js
+++ script.js
@@ -2,6 +2,35 @@ const menuIcon = document.getElementById('menu-icon');
 const responsiveMenu = document.getElementById('menu-responsive');
 const header = document.getElementById('header');
 const volverArriba = document.getElementById('volver-arriba');
+const menuElement = document.getElementById('responsive-menu-element');
+
+
+//menu responsive
+
+const openMenu = () => {
+    menuIcon.classList.replace('bx-menu', 'bx-x');
+    responsiveMenu.style.display = 'flex';
+}
+
+const closeMenu = () => {
+    menuIcon.classList.replace('bx-x', 'bx-menu');
+    responsiveMenu.style.display = 'none';
+}
+
+menuIcon.addEventListener('click', () => {
+    if (menuIcon.classList.contains('bx-menu')) {
+        openMenu();
+    } else {
+        closeMenu();
+    }
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 800) {
+        closeMenu();
+    }
+});
+
 
 // Volver arriba
 
@@ -18,23 +47,6 @@ volverArriba.addEventListener('click', () => {
         top: 0,
         behavior: 'smooth'
     });
-});
-
-menuIcon.addEventListener('click', () => {
-    if (menuIcon.classList.contains('bx-menu')) {
-        menuIcon.classList.replace('bx-menu', 'bx-x');
-        responsiveMenu.style.display = 'flex';
-    } else {
-        menuIcon.classList.replace('bx-x', 'bx-menu');
-        responsiveMenu.style.display = 'none';
-    }
-});
-
-window.addEventListener('resize', () => {
-    if (window.innerWidth > 800) {
-        responsiveMenu.style.display = 'none';
-        menuIcon.classList.replace('bx-x', 'bx-menu');
-    }
 });
 
 
